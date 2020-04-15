3eff204877f12546ae1fd94465307103bc2865a4
saron4 <saronfantahun9@gmail.com>
2020-04-14 22:43:38 -0500

    batch shedule

--- src/main/java/miu/group3/api/repositories/ProductRepository.java
+++ src/main/java/miu/group3/api/repositories/ProductRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import miu.group3.api.documents.Product;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
+	
+	List<Product> product = 
 
 }
